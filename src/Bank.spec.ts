7f6de15a0c36fe2af37b17d13c4742b9eb1a53cc
Garethp <blackfireweb@gmail.com>
2023-02-10 07:03:52 +0000

    Implement BankAccount and add tests for Bank

--- src/Bank.spec.ts
+++ src/Bank.spec.ts
@@ -1,5 +1,5 @@
 import { BankAccountEvent } from "./BankAccount";
-import { hydrateBank } from "./Bank";
+import { BankEvent, hydrateBank } from "./Bank";
 
 describe("All Accounts Stream", () => {
   it("should handle events for multiple accounts", () => {
@@ -18,6 +18,23 @@ describe("All Accounts Stream", () => {
     expect(hydrateBank(events)).toEqual({
       "1": { balance: 25, status: "closed", owner: "Alice" },
       "2": { balance: 100, status: "closed", owner: "Bob" },
+    });
+  });
+
+  it("should transfer money between two accounts", () => {
+    const events: BankEvent[] = [
+      { type: "AccountOpened", data: { accountId: "1", owner: "Alice" } },
+      { type: "AccountOpened", data: { accountId: "2", owner: "Bob" } },
+      { type: "DepositMade", data: { accountId: "2", amount: 200 } },
+      {
+        type: "TransferMade",
+        data: { fromAccountId: "2", toAccountId: "1", amount: 50 },
+      },
+    ];
+
+    expect(hydrateBank(events)).toEqual({
+      "1": { balance: 50, status: "open", owner: "Alice" },
+      "2": { balance: 150, status: "open", owner: "Bob" },
     });
   });
 });
---
+++ src/Bank.ts
@@ -0,0 +1,60 @@
+import {
+  BankAccountEvent,
+  bankAccountEventHandler,
+  BankAccountState,
+} from "./BankAccount";
+
+type BankState = {
+  [accountId: string]: BankAccountState;
+};
+
+export type BankEvent =
+  | BankAccountEvent
+  | {
+      type: "TransferMade";
+      data: { fromAccountId: string; toAccountId: string; amount: number };
+    };
+
+export const bankEventHandler = (
+  state: BankState,
+  event: BankEvent
+): BankState => {
+  switch (event.type) {
+    case "AccountOpened":
+    case "DepositMade":
+    case "WithdrawalMade":
+    case "AccountClosed":
+      return {
+        ...state,
+        [event.data.accountId]: bankAccountEventHandler(
+          state[event.data.accountId],
+          event
+        ),
+      };
+    case "TransferMade":
+      const fromAccount = state[event.data.fromAccountId];
+      const toAccount = state[event.data.toAccountId];
+      if (fromAccount.status === "closed" || toAccount.status === "closed") {
+        throw new Error("Cannot transfer between closed accounts");
+      }
+      if (fromAccount.balance < event.data.amount) {
+        throw new Error("Not enough funds to transfer");
+      }
+      return {
+        ...state,
+        [event.data.fromAccountId]: {
+          ...fromAccount,
+          balance: fromAccount.balance - event.data.amount,
+        },
+        [event.data.toAccountId]: {
+          ...toAccount,
+          balance: toAccount.balance + event.data.amount,
+        },
+      };
+    default:
+      throw new Error(`Unhandled event: ${event}`);
+  }
+};
+
+export const hydrateBank = (events: BankEvent[]) =>
+  events.reduce(bankEventHandler, {});
